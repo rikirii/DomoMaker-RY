1f891c3fb5f614076fb198cd8da5f9881923e4b6
Ricky <ry5665@rit.edu>
2026-04-06 00:56:56 -0400

    DomoMaker C

--- server/router.js
+++ server/router.js
@@ -1,18 +1,24 @@
 const controllers = require('./controllers');
+const mid = require('./middleware');
 
+
+// The way router level middleware in Express works is you connect as many
+// middleware calls as you want in the order you want the middleware to run. The first
+// parameter is always the URL. The last parameter is always the controller. Everything
+// in between is any of the middleware operations you want to call.
 const router = (app) =>{
-    app.get('/login', controllers.Account.loginPage);
-    app.post('/login', controllers.Account.login);
+    app.get('/login', mid.requiresSecure, mid.requiresLogout,  controllers.Account.loginPage);
+    app.post('/login',  mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
-    app.get('/signup', controllers.Account.signupPage);
-    app.post('/signup', controllers.Account.signup);
+    app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+    app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 
-    app.get('/logout', controllers.Account.logout);
+    app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
-    app.get('/maker', controllers.Domo.makerPage);
-    app.post('/maker', controllers.Domo.makeDomo);
+    app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+    app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
 
-    app.get('/', controllers.Account.loginPage);
+    app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 
 };
 
